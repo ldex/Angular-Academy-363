f5add72474456d5a364245692de8e28c7116d800
ldex <laurent.duveau+git@gmail.com>
2023-04-19 15:53:57 -0400

    Product Service

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+
+import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.interface';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  title: string = 'Products';
+  products: Product[];
+  selectedProduct: Product;
+
+  onSelect(product: Product) {
+    this.selectedProduct = product;
+  }
+
+  constructor(
+    private productService: ProductService
+  ) {
+    this.products = productService.getProducts();
+  }
+
+}
